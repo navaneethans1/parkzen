e693885ef1a8d47f9d4fe5e3a22dcbbac644fbc9
Ankit <sainiak009@gmail.com>
2018-05-05 12:09:25 +0530

    Added address in location model

--- app/models.py
+++ app/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Location(models.Model):
+    address = models.TextField()
     lat = models.CharField(max_length=12)
     lon = models.CharField(max_length=12)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.address
 
 
 class TimeSlot(models.Model):
@@ -15,3 +19,6 @@ class TimeSlot(models.Model):
         User, on_delete=models.CASCADE, blank=True, null=True)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     rate = models.CharField(max_length=8)
+
+    def __str__(self):
+        return self.location.address + "-" + self.rate
