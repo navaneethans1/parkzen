a908a963aa52d4b2b34a2b3c1b18e56cb1856ea1
Ankit <sainiak009@gmail.com>
2018-05-06 13:41:50 +0530

    Added cards-distance-sorted for search results

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from app.models import *
 import googlemaps
-from datetime import datetime
+# from datetime import datetime
 import json
 
 gmaps = googlemaps.Client(key='AIzaSyAUyjrixhINVBCtKpOegkjixG7DovoOnmE')
@@ -20,23 +20,41 @@ def search(request):
 @csrf_exempt
 def get_time_slots(request):
     if request.method == "POST":
-        if not request.POST['address'] and request.POST['start'] and request.POST['end']:
-            HttpResponse("Please select preferences")
-        position = request.POST['address'].lower()
+        if not request.POST['address'] \
+                and request.POST['start'] and request.POST['end']:
+            return HttpResponse("Please select preferences")
+        try:
+            data = json.loads(request.POST['address'])
+        except ValueError:
+            position = request.POST['address'].lower()
+            geocode_result = gmaps.geocode(position)
+            data = geocode_result[0]['geometry']['location']
+
         start = request.POST['start']
         end = request.POST['end']
-        geocode_result = gmaps.geocode(position)
         time_slots = TimeSlot.objects.filter(start__gte=start, end__lte=end)
-        data = geocode_result[0]
         available_slots = []
         for time_slot in time_slots:
-            if ((abs(time_slot.location.lat - data['geometry']['location']['lat']) < 0.01)
-                    and (abs(time_slot.location.lon - data['geometry']['location']['lng']) < 0.01)):
-                distance = gmaps.distance_matrix(origins={'lat':time_slot.location.lat, 'lng':time_slot.location.lon}, destinations=data['geometry']['location'], mode="driving")
+            if ((abs(time_slot.location.lat - data['lat']) < 0.01) and (abs(time_slot.location.lon - data['lng']) < 0.01)):
+                distance = gmaps.distance_matrix(origins={
+                                                 'lat': time_slot.location.lat,
+                                                 'lng': time_slot.location.lon},
+                                                 destinations=data,
+                                                 mode="walking")
                 time_slot.distance = distance['rows'][0]['elements'][0]['distance']['text']
                 time_slot.duration = distance['rows'][0]['elements'][0]['duration']['text']
                 available_slots.append(time_slot)
+        available_slots.sort(key=lambda x: x.distance, reverse=False)
         if len(available_slots) <= 0:
             return HttpResponse("No available parking slots :(")
         return render(request, 'slots-user-table.html',
                       {'available_slots': available_slots})
+
+
+# @csrf_exempt
+# def book_slot(request):
+#     if request.method == "POST":
+#         if not if not request.POST['slot_id']:
+#             return HttpResponse("Please select slot")
+#         user = User.objects.get()
+
